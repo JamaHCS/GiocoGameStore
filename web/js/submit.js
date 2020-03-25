f5d8bb32ef2804f4d2f082e37e7848ed6d6904ca
JamaHCS <jama117@hotmail.com>
2020-03-25 14:16:03 -0600

    kk x2

--- web/js/submit.js
+++
@@ -1,4 +0,0 @@
-const submit = document.getElementById('submitFormLogin');
-const user = document.getElementById('userInput');
-const pass = document.getElementById('passwordInput');
-submit.onclick = () => alert('Usuario: ' + user.value + ' contraseña:' + pass.value);
